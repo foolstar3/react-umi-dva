2e9e62d4c747a57303e057a320549b6e4bcd5858
jing.gong <jing.gong@united-imaging.com>
2021-08-25 14:35:42 +0800

    修改test、dev环境host、port和url

--- .umirc.test.ts
+++ .umirc.test.ts
@@ -9,11 +9,11 @@ export default defineConfig({
   define: {
     // test 环境变量
     'process.env.var': 'test',
-    'process.env.qcFrontUrl': 'QC_FRONT_URL_TEST',
+    'process.env.qcFrontUrl': 'http://10.6.209.209:9999',
   },
   devServer: {
     port: 9000,
-    host: '127.0.0.1',
+    host: '10.6.209.209',
   },
   hash: true,
   history: {
